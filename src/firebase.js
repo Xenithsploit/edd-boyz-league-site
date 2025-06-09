e1a0d37e39ad00bc5aa80edbe8ea70376ef6b3bd
Justin Roseborough <justin.j.roseborough@gmail.com>
2025-06-08 23:09:55 -0500

    Initial commit for EDD Boyz Network v2

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore'; // Import Firestore
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDExNgraITaJpcA8eFhyDbdfj51aRxpwnI",
-  authDomain: "edd-boyz-network.firebaseapp.com",
-  projectId: "edd-boyz-network",
-  storageBucket: "edd-boyz-network.firebasestorage.app",
-  messagingSenderId: "726747548460",
-  appId: "1:726747548460:web:f64099bd104dff7a08ba89",
-  measurementId: "G-ZE5Y9BQXVZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
